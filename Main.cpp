5cde31be608b68b27886045d9c95322738675972
Ian Carlos Azevedo Martins <iancarlos335@gmail.com>
2023-09-24 23:33:04 -0300

    Ajustando dessa forma por enquanto, só pra entregar o trabalho logo

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 #include <locale>
 #include <clocale>
 using namespace std;
@@ -12,7 +14,7 @@ double privateImc;
 
 bool loopController = true;
 int menuController = 0;
-bool loopSkip = true;
+bool skipLoop = false;
 
 class Pessoa
 {
@@ -39,18 +41,19 @@ vector<Pessoa> pessoas;
 
 void showMenu()
 {
-    cout << "\nVoce gostaria de cadastrar mais uma pessoa, listar as jÃ¡ cadastradas, ou encerrar o programa?\n\n1 - Cadastrar mais uma pessoa\n2 - Listar cadastradas \n3 - Filtrar pessoas pelo nome\n4 - Encerrar o programa\n\n";
+    cout << "\nVoce gostaria de cadastrar mais uma pessoa, listar as já cadastradas, ou encerrar o programa?\n\n1 - Cadastrar mais uma pessoa\n2 - Listar cadastradas \n3 - Filtrar pessoas pelo nome\n4 - Encerrar o programa\n\n";
     cin >> menuController;
 
     if (cin.fail())
-        throw(1);
+        throw(2);
 
     switch (menuController)
     {
-    case 1: // Dou sequÃªncia no loop e forÃ§o a introduÃ§Ã£o ser reapresentada.
-        loopSkip = true;
+    case 1: // Dou sequência no loop e forço a introdução ser reapresentada.
+        peopleRegistration();
+        skipLoop = false;
         break;
-    case 2: //  Pego todas as pessoas e exibo cada atributo. O cadastro de Pessoa Ã© pulado tambÃ©m.
+    case 2: //  Pego todas as pessoas e exibo cada atributo. O cadastro de Pessoa é pulado também.
     {
         for (const Pessoa &obj : pessoas)
         {
@@ -62,27 +65,33 @@ void showMenu()
             cout << "Altura: " << obj.altura << "m" << endl;
             cout << "IMC: " << obj.imc << endl;
         }
-        loopSkip = false;
+        skipLoop = true;
         break;
     }
     case 3: // Filtro pelo nome
     {
         cout << "Insira o nome a ser filtrado:" << endl;
-        cin >> filter;
+        getline(cin >> ws, filter);
 
         if (cin.fail())
-            throw(1);
+            throw(2);
 
         vector<Pessoa> pessoasFiltradas;
 
         for (const Pessoa &obj : pessoas)
         {
-            if (obj.nome.find(filter) != string::npos)
+            string nomeUpperCased = obj.nome;
+            // Filtra sem ser case sensitive
+            transform(nomeUpperCased.begin(), nomeUpperCased.end(), nomeUpperCased.begin(), ::toupper);
+            transform(filter.begin(), filter.end(), filter.begin(), ::toupper);
+
+            if (nomeUpperCased.find(filter) != string::npos)
             {
                 pessoasFiltradas.push_back(obj);
             }
         }
 
+        // Exibo o Filtro
         for (const Pessoa &obj : pessoasFiltradas)
         {
             cout
@@ -93,54 +102,56 @@ void showMenu()
             cout << "Altura: " << obj.altura << "m" << endl;
             cout << "IMC: " << obj.imc << endl;
         }
-        loopSkip = false;
+        skipLoop = true;
 
         break;
     }
     case 4: // Encerro o programa, matando o loop.
         loopController = false;
-        loopSkip = false;
+        skipLoop = false;
         break;
-    default: // Se no menu eu receber um nÃºmero diferente dos esperados essa mensagem Ã© exibida. O cadastro de Pessoa Ã© pulado tambÃ©m.
-        cout << "O nÃºmero digitado nao compreende aos esperados\n";
-        loopSkip = false;
+    default: // Se no menu eu receber um número diferente dos esperados essa mensagem é exibida. O cadastro de Pessoa é pulado também.
+        cout << "O número digitado nao compreende aos esperados\n";
+        skipLoop = true;
         break;
     };
 }
 
+void peopleRegistration()
+{
+    // Inserção de dados
+    cout << "Insira seu nome abaixo:" << endl;
+    getline(cin >> ws, privateNome);
+    cout << "Insira a sua idade:" << endl;
+    cin >> privateIdade;
+    cout << "Insira o seu peso:" << endl;
+    cin >> privatePeso;
+    cout << "Insira a sua altura:" << endl;
+    cin >> privateAltura;
+
+    if (cin.fail())
+        throw(1);
+
+    // Calculo de IMC
+    privateImc = privatePeso / (privateAltura * privateAltura);
+    cout << "\nSeu imc é: " << privateImc << endl;
+
+    // Cadastro da Pessoa
+    Pessoa pessoa(privateNome, privateIdade, privatePeso, privateAltura, privateImc);
+    pessoas.push_back(pessoa);
+}
+
 int main()
 {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
-    locale::global(locale());
-    cout.imbue(locale());
+    setlocale(LC_ALL, "Portuguese_Brazil");
 
     try
     {
         while (loopController)
         {
-            if (loopSkip)
+            if (!skipLoop)
             {
-                // InserÃ§Ã£o de dados
-                cout << "Insira seu nome abaixo:" << endl;
-                getline(cin >> ws, privateNome);
-                cout << "Insira a sua idade:" << endl;
-                cin >> privateIdade;
-                cout << "Insira o seu peso:" << endl;
-                cin >> privatePeso;
-                cout << "Insira a sua altura:" << endl;
-                cin >> privateAltura;
-
-                if (cin.fail())
-                    throw(1);
-
-                // Calculo de IMC
-                privateImc = privatePeso / (privateAltura * privateAltura);
-                cout << "\nSeu imc Ã©: " << privateImc << endl;
-
-                // Cadastro da Pessoa
-                Pessoa pessoa(privateNome, privateIdade, privatePeso, privateAltura, privateImc);
-                pessoas.push_back(pessoa);
-
+                peopleRegistration();
                 showMenu();
             }
             else
@@ -151,8 +162,17 @@ int main()
     }
     catch (int meuErro)
     {
-        if (meuErro == 1)
-            cout << "\n\nO que foi digitado nÃ£o era o esperado. Apenas nÃºmeros sÃ£o permitidos.\n\n";
+        switch (meuErro)
+        {
+        case 1: // Exception no Cadastro
+            cout << "\n\nO que foi digitado não era o esperado. Apenas números são permitidos.\n\n";
+            break;
+        case 2: // Exception no Menu
+            cout << "\n\nO que foi digitado não era o esperado. Apenas números são permitidos.\n\n";
+            break;
+        }
+
+        cin.clear();
     }
 
     return 0;
